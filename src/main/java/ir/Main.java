d17aa1d5be75a19234a59ede6ca73b4f52107e42
mehdi.khorrami <mehdi.khorrami78@gmail.com>
2023-09-17 15:11:05 +0330

    finish work by stream

--- src/main/java/ir/Main.java
+++ src/main/java/ir/Main.java
@@ -21,7 +21,7 @@ public class Main {
             person = file.skip(1)
                     .map(i -> {
                         String[] s = i.split(",");
-                        return new Person(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9] );
+                        return new Person(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9]);
                     }).toList();
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,32 +29,76 @@ public class Main {
 
 
         //Person objects to include only those where the age is between 25 and 32(inclusive)
-/*        person.stream()
+        person.stream()
                 .filter(i -> Integer.parseInt(i.getAge()) >= 25 && Integer.parseInt(i.getAge()) <= 32)
-                .forEach(System.out::println);*/
-
-
+                .forEach(System.out::println);
 
 
         // Map the filtered list to new list and write PersonsEmailAddresses.txt file
-/*        List<String> emails = person.stream()
+        List<String> emails = person.stream()
                 .map(i -> i.getEmail())
                 .sorted()
                 .toList();
 
-        try (FileWriter fw = new FileWriter("PersonsEmailAddresses.txt" )){
-            for (String s: emails) {
+        try (FileWriter fw = new FileWriter("PersonsEmailAddresses.txt")) {
+            for (String s : emails) {
                 fw.write(s);
                 fw.append("\n");
             }
             fw.flush();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
 
         //Calculate and print the average salary of all Person objects in the list.
-        /*System.out.println("average Salary is : " + Calculate.averageSalary(person));*/
+        System.out.println("average Salary is : " + Calculate.averageSalary(person));
+
+
+        //Find and print the Person object with the highest salary.
+        System.out.println(Calculate.highestSalary(person));
+
+
+        // Determine and print the count of Person objects for each unique geography value
+        List<String> gh = person.stream()
+                .map(i -> i.getGeography())
+                .distinct()
+                .toList();
+
+        for (int l = 0; l < gh.size(); l++) {
+            int finalL = l;
+            Long co = person.stream()
+                    .filter(i -> i.getGeography().equals(gh.get(finalL)))
+                    .count();
+            System.out.println(gh.get(finalL) + " count is : " + co);
+        }
+
+
+        //Find and print the number of males and females in the list
+        System.out.println(Calculate.numberOfType(person, "Male"));
+        System.out.println(Calculate.numberOfType(person, "Female"));
+
+        //Filter the list to include only alive persons and calculate the total salary of those persons
+        System.out.println(Calculate.averageSalaryAlive(person));
+
+
+        //Calculate and print the total salary for each unique profession in the list
+        List<String> un = person.stream()
+                .map(i -> i.getProfession())
+                .distinct()
+                .toList();
+
+        for (int l = 0; l < un.size(); l++) {
+            int finalL = l;
+            Long lg = person.stream()
+                    .filter(i -> i.getProfession().equals(un.get(finalL)))
+                    .map(i -> Long.parseLong(i.getSalary()))
+                    .reduce((i, j) -> i + j)
+                    .get();
+
+            System.out.println(un.get(finalL) + " is : " + lg);
+        }
+
 
     }
 }
--- src/main/java/ir/calculate/Calculate.java
+++ src/main/java/ir/calculate/Calculate.java
@@ -3,7 +3,7 @@ package ir.calculate;
 import ir.modals.Person;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public class Calculate {
 
@@ -13,4 +13,38 @@ public class Calculate {
                 .average()
                 .getAsDouble();
     }
+
+    public static Person highestSalary(List<Person> person) {
+
+        return person.stream()
+                .reduce((a, b) -> {
+                    if (Integer.parseInt(a.getSalary()) > Integer.parseInt(b.getSalary())) {
+                        return a;
+                    }
+                    return b;
+                })
+                .get();
+    }
+
+    public static Long numberOfType(List<Person> person, String gender) {
+
+        return person.stream()
+                .filter(i -> i.getGender().equals(gender))
+                .count();
+
+    }
+
+    public static Double averageSalaryAlive(List<Person> person) {
+
+        List<Person> alivePerson = person.stream()
+                .filter(i -> i.getIsAlive().equals("0"))
+                .toList();
+
+        //System.out.println(alivePerson.size());
+
+        return averageSalary(alivePerson);
+
+    }
+
+
 }
