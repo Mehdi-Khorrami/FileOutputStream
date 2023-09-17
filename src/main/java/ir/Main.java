4c4cbad036c7a947b431faa236af20e3e2572868
mehdi.khorrami <mehdi.khorrami78@gmail.com>
2023-09-17 10:48:35 +0330

    write person class and read file

--- src/main/java/ir/Main.java
+++ src/main/java/ir/Main.java
@@ -1,7 +1,32 @@
 package ir;
 
+import ir.modals.Person;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<Person> person = new ArrayList<>();
+
+        try (Stream<String> file = Files.lines(Paths.get("./StreamPracticeWithCSVFile.csv"))) {
+            person = file.skip(1)
+                    .map(i -> {
+                        String[] s = i.split(",");
+                        return new Person(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9]);
+                    }).toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
---
+++ src/main/java/ir/modals/Person.java
@@ -0,0 +1,125 @@
+package ir.modals;
+
+public class Person {
+
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String age;
+    private String gender;
+    private String geography;
+    private String email;
+    private String profession;
+    private String salary;
+    private String isAlive;
+
+
+    public Person(String id, String firstname, String lastname, String age, String gender, String geography, String email, String profession, String salary, String isAlive) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.gender = gender;
+        this.geography = geography;
+        this.email = email;
+        this.profession = profession;
+        this.salary = salary;
+        this.isAlive = isAlive;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGeography() {
+        return geography;
+    }
+
+    public void setGeography(String geography) {
+        this.geography = geography;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(String isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", geography='" + geography + '\'' +
+                ", email='" + email + '\'' +
+                ", profession='" + profession + '\'' +
+                ", salary=" + salary +
+                ", isAlive=" + isAlive +
+                '}';
+    }
+}
